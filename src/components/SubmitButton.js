986ec3ef59c6ea09c9f004003dbebcedc941b8ac
izan <ivozanacchi501@gmail.com>
2024-05-14 20:02:11 -0300

    removingConsoleLogs-deprecatedCode

--- src/components/SubmitButton.js
+++ src/components/SubmitButton.js
@@ -1,4 +1,4 @@
-import { Pressable, StyleSheet, Text, TouchableOpacity } from "react-native";
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
 import React from "react";
 import { LinearGradient } from "expo-linear-gradient";
 
@@ -46,32 +46,5 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
         width: 150,
-        //top: 90,
     },
 });
-
-/*const SubmitButton = ({ onPress, title }) => {
-    return (
-        <Pressable onPress={onPress} style={styles.button}>
-            <Text style={styles.text}>{title}</Text>
-        </Pressable>
-    );
-};
-
-export default SubmitButton;
-
-const styles = StyleSheet.create({
-    button: {
-        //backgroundColor: colors.teal600,
-        borderRadius: 6,
-        justifyContent: 'center',
-        alignItems: 'center',
-        padding: 8,
-        width: '60%'
-    },
-    text: {
-        color: colors.platinum,
-        fontFamily: 'PlayFair',
-        fontSize: 22
-    },
-});*/
